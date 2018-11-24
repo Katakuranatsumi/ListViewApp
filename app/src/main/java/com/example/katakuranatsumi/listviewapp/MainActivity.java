d72b5e3e7c0270ab27d68990600d7bc55a6846f1
Katakuranatsumi <usako7233@gmail.com>
2018-11-24 18:13:34 +0900

    2018/11/24 リストビュー完成

--- app/src/main/java/com/example/katakuranatsumi/listviewapp/MainActivity.java
+++ app/src/main/java/com/example/katakuranatsumi/listviewapp/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.katakuranatsumi.listviewapp;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.EventLog;
+import android.util.LayoutDirection;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,19 +29,138 @@ public class MainActivity extends AppCompatActivity {
         ListView myListView = (ListView) findViewById(R.id.myListView);
 
 //        データを準備
-        ArrayList<String> items = new ArrayList<>();
-        for (int i = 0; i < 30; i++){
-            items.add("item-" + i);
+        ArrayList<User> users = new ArrayList<>();
+
+        final int[] icons = {
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher
+        };
+
+        String[] names = {
+               "Taguchi",
+               "Fkoji",
+                "Dotinstall"
+        };
+
+        String[] locs = {
+                "Ebisu",
+                "Shibuya",
+                "Tokyo"
+        };
+
+        for(int i = 0; i < icons.length; i++){
+            User user = new User();
+            user.setIcon(BitmapFactory.decodeResource(
+                  getResources(),
+                  icons[i]
+            ));
+            user.setName(names[i]);
+            user.setLoc(locs[i]);
+            users.add(user);
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-             this,
-             R.layout.list_view,
-             items
-        );
+//        Adapter - ArrayAdapter - UserAsapter
+        UserAdapter adapter = new UserAdapter(this, 0, users);
 
-//        ListViewに表示
-        myListView.setEmptyView(findViewById(R.id.emptyView));
+        //        ListViewに表示
         myListView.setAdapter(adapter);
+
+//        Event
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(
+                    AdapterView<?> parent,
+                    View view,
+                    int position,
+                    long id) {
+                TextView name = (TextView) view.findViewById(R.id.name);
+                Toast.makeText(
+                  MainActivity.this,
+                   Integer.toString(position) +  ":" + name.getText().toString(),
+                    Toast.LENGTH_SHORT
+                ).show();
+                name.setText("Tapped!");
+            }
+        });
+    }
+
+    public class UserAdapter extends ArrayAdapter<User> {
+
+        private LayoutInflater layoutInflater;
+
+        public UserAdapter(Context c, int id, ArrayList<User> users){
+            super(c, id, users);
+            this.layoutInflater = (LayoutInflater) c.getSystemService(
+              Context.LAYOUT_INFLATER_SERVICE
+            );
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent){
+            ViewHolder holder;
+
+
+            if(convertView == null){
+             convertView = layoutInflater.inflate(
+               R.layout.list_view,
+               parent,
+                     false
+             );
+
+               holder = new ViewHolder();
+               holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+               holder.name = (TextView) convertView.findViewById(R.id.name);
+               holder.loc  = (TextView) convertView.findViewById(R.id.loc);
+               convertView.setTag(holder);
+            } else{
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            User user = (User) getItem(pos);
+
+            holder.icon.setImageBitmap(user.getIcon());
+            holder.name.setText(user.getName());
+            holder.loc.setText(user.getLoc());
+
+            return convertView;
+        }
+    }
+
+    static  class ViewHolder {
+        ImageView icon;
+        TextView  name;
+        TextView  loc;
+    }
+
+    public class User{
+        private Bitmap icon;
+        private String name;
+
+        public String getLoc(){
+            return  loc;
+        }
+
+        public void setLoc(String loc){
+            this.loc = loc;
+        }
+
+        public Bitmap getIcon() {
+            return icon;
+        }
+
+        public void setIcon(Bitmap icon){
+            this.icon = icon;
+        }
+
+        public  String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+        private String loc;
     }
 }
